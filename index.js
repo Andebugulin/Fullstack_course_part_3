d616c75ba74c29e69abe85e61415a24aeb65cf16
Andebugulin <gulin.andrey2005@gmail.com>
2025-05-20 15:16:34 +0300

    exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 // Use morgan middleware with 'tiny' configuration for logging
 app.use(morgan('tiny'));
